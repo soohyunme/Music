212fc6e8d1bd559b7268e0394d2eeb44f86570c4
soohyunme <soohyun.dev@gmail.com>
2023-09-10 02:28:53 +0900

    feat: add cd pipeline

--- src/app.py
+++ src/app.py
@@ -8,7 +8,12 @@ async def read_root():
     return {"message": "Hello, FastAPI!"}
 
 
+@app.get("/health")
+async def healthcheck():
+    return {"status": "healthy"}
+
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
